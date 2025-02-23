6de89e742468ebafaa7600062315bf4cdbae1aac
syx-chongqing <925182053@qq.com>
2025-02-23 18:26:58 +0800

    lab02

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,11 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+    @Test
+    public void testSquarePrimesSimple02() {
+        IntList lst = IntList.of(14, 15, 16, 17, 19);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("14 -> 15 -> 16 -> 289 -> 361", lst.toString());
+        assertTrue(changed);
+    }
 }
