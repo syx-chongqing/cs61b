7dc9d8b3e0db11b5ca679a7a511a471054059764
syx-chongqing <925182053@qq.com>
2025-03-17 19:12:25 +0800

    buggy lab4

--- lab4/Collatz.java
+++ lab4/Collatz.java
@@ -1,13 +1,13 @@
 public class Collatz {
 
-    /**implementation of nextNumber! */
+    /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 1) {
+        if (n  == 128) {
             return 1;
-        } else if (n % 2 == 0) {
-            return n / 2;
+        } else if (n == 5) {
+            return 3 * n + 1;
         } else {
-            return n * 3 + 1;
+            return n * 2;
         }
     }
 
