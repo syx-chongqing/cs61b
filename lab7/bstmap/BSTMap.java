d0a3362fcf7e1aa5db7029d3df0fe7a9f0f8672a
syx-chongqing <925182053@qq.com>
2025-04-03 14:25:33 +0800

    lab7

---
+++ lab7/bstmap/BSTMap.java
@@ -0,0 +1,125 @@
+package bstmap;
+
+import java.util.Iterator;
+import java.util.Set;
+
+public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
+    private BSTNode root;
+    @Override
+    public void clear() {
+        root = null;
+    }
+    public BSTMap() {
+
+    }
+    private class BSTNode {
+        private K key;
+        private V value;
+        private BSTNode left, right;
+        private int size;
+        public BSTNode(K key, V value, int size) {
+            this.key = key;
+            this.value = value;
+            this.size = size;
+        }
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        if (key == null) {
+            throw new IllegalArgumentException("argument to contains() is null");
+        }
+        return containsKey(root, key);
+    }
+    private boolean containsKey(BSTNode x, K key) {
+        if (x == null) {
+            return false;
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            return containsKey(x.left, key);
+        } else if (cmp > 0) {
+            return containsKey(x.right, key);
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public V get(K key) {
+        return get(root, key);
+    }
+    private V get(BSTNode x, K key) {
+        if (key == null) {
+            throw new IllegalArgumentException("calls get() with a null key");
+        }
+        if (x == null) {
+            return null;
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            return get(x.left, key);
+        } else if (cmp > 0) {
+            return get(x.right, key);
+        } else {
+            return x.value;
+        }
+    }
+
+    @Override
+    public int size() {
+        return size(root);
+    }
+    private int size(BSTNode x) {
+        if (x == null) {
+            return 0;
+        }
+        return x.size;
+    }
+
+    @Override
+    public void put(K key, V value) {
+        if (key == null) {
+            throw new IllegalArgumentException("calls put() with a null key");
+        }
+
+        root = put(root, key, value);
+
+    }
+    private BSTNode put(BSTNode x, K key, V value) {
+        if (x == null) {
+            return new BSTNode(key, value, 1);
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            x.left = put(x.left, key, value);
+        } else if (cmp > 0) {
+            x.right = put(x.right, key, value);
+        } else {
+            x.value = value;
+        }
+        x.size = 1 + size(x.left) + size(x.right);
+        return x;
+
+    }
+
+    @Override
+    public Set<K> keySet() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V remove(K key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V remove(K key, V value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        throw new UnsupportedOperationException();
+    }
+}
